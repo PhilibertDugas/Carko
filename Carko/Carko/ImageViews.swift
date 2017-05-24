dfb2351c6423613a2c6808223da0d5ae1fda2e2e
Philibert Dugas <philibert.dugas@gmail.com>
2017-05-24 12:25:56 -0400

    New logo - take 2. Light themed

---
+++ Carko/Carko/ImageViews.swift
@@ -0,0 +1,23 @@
+//
+//  ImageViews.swift
+//  Carko
+//
+//  Created by Philibert Dugas on 2017-05-24.
+//  Copyright © 2017 QH4L. All rights reserved.
+//
+
+import UIKit
+
+class RoundedCornerImageView: UIImageView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 3
+        layer.borderWidth = 0.5
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 3
+        layer.borderWidth = 0.5
+    }
+}
